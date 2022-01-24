ba7eca43820fc911fae0a296ddf4a085d374a3a3
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-01-24 20:26:27 -0300

    main

--- Modulo 1/Aula 6/aula6.js
+++ Modulo 1/Aula 6/aula6.js
@@ -30,6 +30,25 @@ do {
 } while (continuar == 'S')
 */
 
+//o break é uma mnaneira de interromper minha repetição antes mesmo de terminar toda a execução do codigo e testes
+//O comando break encerra o loop atual, independente do resultado do teste da condição.
+
+/*
+let senha = '';
+let tentativas = 0;
+
+do {
+    if (tentativas === 5){
+        console.log("Número limite de tentativas atingido.");
+        break;
+    } 
+
+    senha = prompt("Digite a senha de acesso: ");
+    tentativas++;
+} while (senha !== "Blue");
+*/
+
+
 //exemplo 1
 /*
 let nome = '';
@@ -49,6 +68,7 @@ while (nome == ''){
 
 
 //exemplo 2
+/*
 let jogo = 'sim';
 
 while (jogo == 'sim'){
@@ -63,3 +83,59 @@ while (jogo == 'sim'){
     console.log('Acabou o jogo');
     jogo = prompt ('Quer jogar de novo?');
 }
+*/
+
+//CONTINUE
+/*
+A palavra chave continue termina a atual iteração do laço em 
+que ele se encontra, e continua a execução deste 
+laço com a próxima iteração. É como o break, mas ao invés de 
+encerrar e sair da laço, ele encerra a repetição atual ignorando
+ todos os comando que viriam depois, e volta para o início com 
+ a próxima execução.
+*/
+
+/*
+let n = 0;
+
+while (n < 5) {
+    n++;
+ 
+    if (n == 3) {
+        continue;
+    }
+
+    console.log(n);
+}
+*/
+
+
+/*
+let jogo = 'sim';
+
+while (jogo == 'sim'){
+    console.log('Voce esta jogadno');
+    console.log('Voce esta em perigo');
+    console.log('Voce foi atacado por uma cobra');
+    let antidoto = prompt ('voce tem um antidoto?');
+    if (antidoto == 'n'){
+        console.log('que pena. VOce morreu');
+        continuar = prompt('deseja jogar novamente?')
+        if (continuar == 's'){
+            console.log('RECOMECANDO')
+            continue;
+        }
+        else{
+            console.log('Obrigado por jogar')
+            break;
+        }
+    }
+    console.log('que bom que vc tinha o antidoto');
+    console.log('voce continua sua jornada');
+    console.log('Acabou o jogo');
+    jogo = prompt ('Quer jogar de novo?');
+}
+*/
+
+
+//AULA 6 comeca aqui:
