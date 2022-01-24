1b23e72f26780bdd30db3b6cd7aadeda89264d3b
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-01-24 20:04:59 -0300

    main

--- Modulo 1/Aula 6/aula6.js
+++ Modulo 1/Aula 6/aula6.js
@@ -1,10 +1,65 @@
 const prompt = require('prompt-sync')();
 
 //revisao While
-let n1 = 0;
+// let n1 = 0;
 
-while (n1 <5){
-    console.log('é verdadeiro');
-    n1++;
-} 
+/* while (n1 <5){
+     console.log('é verdadeiro');
+     n1++;
+ } 
+*/
 
+/*
+ let continuar = 's';
+
+ while (continuar == 's'){
+     console.log('Todo meu jogo');
+     
+     continuar = prompt('Deseja jogar novamente? ')
+ } 
+*/
+
+
+/*
+let continuar = 's';
+
+do {
+    console.log('Todo meu jogo');
+    
+    continuar = prompt('Deseja jogar novamente? ')
+} while (continuar == 'S')
+*/
+
+//exemplo 1
+/*
+let nome = '';
+
+while (nome == ''){
+    console.log('Bem vindo ao cadastro da loja!');
+    let continuar = prompt('Voce já possui cadastro? ');
+    if (continuar == 's'){
+        break;
+    }
+    nome = prompt('Digite seu nome: ')
+    while (nome.length < 3){
+        console.log('Nome invalido');
+        nome = prompt('Digite seu nome: ')
+    }
+} */
+
+
+//exemplo 2
+let jogo = 'sim';
+
+while (jogo == 'sim'){
+    console.log('Voce esta jogadno');
+    console.log('Voce esta em perigo');
+    console.log('Voce foi atacado por uma cobra');
+    let antidoto = prompt ('voce tem um antidoto?');
+    if (antidoto == 'n'){
+        console.log('que pena.')
+        break;
+    }
+    console.log('Acabou o jogo');
+    jogo = prompt ('Quer jogar de novo?');
+}
