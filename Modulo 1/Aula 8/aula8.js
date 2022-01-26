7e80ad11dc802127f62b352bb77728a6653cea3a
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-01-26 19:17:00 -0300

    main

---
+++ Modulo 1/Aula 8/aula8.js
@@ -0,0 +1,3 @@
+/*Uma expressão do tipo for cria um loop que vai repetir uma declaração por uma quantidade (geralmente) fixa de vezes.
+As três principais formas de utilização do for são o for (também conhecido como for indexado) o for of e o for in.
+*/
