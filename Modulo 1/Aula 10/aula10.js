717f5672cac311d3d5cce4abf010d9e1128e4f51
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-01-28 20:35:20 -0300

    main

--- Modulo 1/Aula 10/aula10.js
+++ Modulo 1/Aula 10/aula10.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 
 // let pergunta = prompt ('Responda com s ou n: ');
-let numero = prompt('Digite um numero')
+/*let numero = prompt('Digite um numero')
 //var eu posso redeclarar e let eu nao posso alterar
 
 
@@ -23,3 +23,41 @@ while (isNaN(numero)){//só aceita numero
 while (!isNaN(numero)){//só aceita string 
     numero = prompt ('Digite um valor valido!')
 }
+
+*/
+// array pra maisculo
+/*
+let lista = ['joao', 'maria', 'fernando'];
+
+let listamaisculo = [];
+let stringvazia = '';
+
+for (let i = 0 ; lista.length; i++){
+    stringvazia = lista[i];
+    stringvazia = stringvazia.toLocaleUpperCase();
+    listamaisculo.push(stringvazia);
+}
+*/
+//forma 2 (mais simples):
+/*
+let lista = ['Gabriel', 'Joao Vitor', 'Carlos'];
+let listaUpper = [];
+
+for(let elemento in lista)
+{
+    listaUpper.push(lista[elemento].toUpperCase());
+}
+console.log(listaUpper);
+*/
+
+//forma 3:
+/*
+let lista = ['Gabriel', 'Joao Vitor', 'Carlos'];
+let listaUpper = [];
+
+for(let elemento in lista)
+{
+    listaUpper.push(elemento.toUpperCase());
+}
+console.log(listaUpper);
+*/
