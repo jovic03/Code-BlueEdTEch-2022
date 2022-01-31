d88415d2eceda840f53f632a0b4b2202c87d5a43
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-01-31 20:00:07 -0300

    main

--- Modulo 1/Aula 11/aula11.js
+++ Modulo 1/Aula 11/aula11.js
@@ -39,3 +39,59 @@ for (let i of lista){// percorre a lista
 console.log(menor);
 */
 
+/*
+A declaração function define uma função com os especificados parâmetros. É um conjunto 
+de instruções que executa uma tarefa ou calcula um valor. Para usar uma função, você deve 
+defini-la em algum lugar no escopo do qual você quiser chamá-la.
+É como se ensinássemos para o computador uma série de comandos que queremos executar chamando apenas 
+por um nome. Isso é muito útil quando precisamos executar um certo código várias vezes durante a 
+execução do nosso programa, então ao invés de repetir várias vezes esses códigos, criamos uma função para facilitar.
+*/
+
+
+function va_tomar_banho (){
+    console.log('Tirar a roupa suja');
+    console.log('Abrir o chuveriro')
+    console.log('Se ensaboar')
+    console.log('Lavar o cabelo')
+    console.log('Lavar o suvaco')
+    console.log('Se enxaguar')
+    console.log('Fechar o chuveriro')
+    console.log('Se secar')
+    console.log('Colocar uma roupa limpa')
+    
+
+}
+
+// let sujo = prompt ('O Henrique esta sujo? ');
+
+// if(sujo =='s'){
+//     va_tomar_banho();
+// }
+
+// sujo = prompt ('O Marcos esta sujo? ');
+
+// if(sujo =='s'){
+//     va_tomar_banho();
+// }
+
+// sujo = prompt ('O Iulia esta sujo? ');
+
+// if(sujo =='s'){
+//     va_tomar_banho();
+// }
+
+function verifica_sujeira(){
+    let sujo = prompt ('Está sujo(a)? ')
+    if(sujo =='s'){
+    va_tomar_banho();
+}
+
+}
+
+let pessoas = +prompt('Quantas pessoas estao em casa? ');
+
+for (let i =- 0; i<pessoas;i++){
+    verifica_sujeira();
+}
+
