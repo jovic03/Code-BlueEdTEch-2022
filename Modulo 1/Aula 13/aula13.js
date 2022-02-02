88b700930e00ee0ad1903edb03ec5567a383801d
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-02-02 20:00:57 -0300

    main

--- Modulo 1/Aula 13/aula13.js
+++ Modulo 1/Aula 13/aula13.js
@@ -2,6 +2,7 @@ const prompt = require('prompt-sync')();
 
 //como criar eventos aleatorios
 
+/*
 function chuva (){
     console.log('Esta chovendo')
 }
@@ -22,3 +23,99 @@ while(true){
         break;
     }
 }
+*/
+
+//return em function revisao
+
+//exempolo1
+/*
+function soma (a,b){
+    let soma = a +b;
+    console.log(soma);
+}
+soma(13,10)// 'a' recebe 13 e 'b' recebe 10 
+*/
+//exempolo2
+
+/*
+function multi (a,b){
+    let multiplicao = a * b;
+    console.log(multiplicao);
+}
+soma(13,10)// 'a' recebe 13 e 'b' recebe 10 
+*/
+//exempolo3
+
+/*
+function idade (ano){
+    let idade = 2022 - ano;
+    console.log(idade);
+}
+
+idade(2010);
+*/
+
+
+
+//exeplo 3 com retorno
+/*
+function calculaidade (ano){
+    let idade = 2022 - ano;
+    return idade;
+}
+
+let year = +prompt('Digitye seu ano de nascimento: ')
+let age = calculaidade(year);
+console.log(age)
+*/
+
+
+
+//exeplo 3 com retorno vs2
+/*
+function calculaidade (ano){
+    let idade = 2022 - ano;
+    if (idade>=18){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let year = +prompt('Digitye seu ano de nascimento: ')
+let age = calculaidade(year);
+
+if  (age == true){//por padrao age é true entao nao precisaria colocar o '== true'
+    console.log('voce podew beber')
+}else{
+    console.log('melhor nao beber')
+}
+console.log(age)
+*/
+
+
+//exeplo 3 com retorno vs2 com array
+
+function highlander (ano){
+    let idade = 2022 - ano;
+    if (idade>=2000){
+        console.log('achammos o highlander');
+        let dados = [];
+        dados.push(idade);
+        let nome = prompt ('digite seu nome ');
+        dados.push(nome);
+        let cidade = prompt('digite sua cidade: ');
+        dados.push(cidade);
+        let altura = +prompt('digite sua altura: ')
+        dados.push(altura);
+        
+        return dados;
+
+    }
+}
+
+let year = +prompt('Digite seu ano de nascimento: ')
+// let age = calculaidade(year);
+
+let verifica = highlander(year)
+console.log(verifica)
