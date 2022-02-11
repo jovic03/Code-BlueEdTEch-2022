baf9d1b1c14d453855c06522844df886f4636334
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-02-10 22:12:35 -0300

    main

--- Modulo 1/Projeto Ficcao interativa/ficcao.js
+++ Modulo 1/Projeto Ficcao interativa/ficcao.js
@@ -71,20 +71,70 @@ function testaEscolha(){
 
 function Fnaleatorio(){
     const aleatorio = Math.floor(Math.random() * 10);
-    console.log(aleatorio)
+    console.log(aleatorio);
 }
 
 
 
-    console.log('Você acaba de acordar de uma noite mal dormida como de costuma')
-    console.log('Sua esposa, Ada Lovelace está com você na cama, e seu animal elétrico, a ovelha Dolly, está a porta.')
+function androideMorre(){
+    console.log(`Você acerta o tiro e a mata!`);
+    console.log(`Leva o corpo até a delegacia para receber sua recompensa na esperança de que aquele dinheiro se transforme em algo que preencha seu existir.`);
+    console.log(`Mas o inesperado ocorre:`);
+    royBattyVengance();
+    fim();
+}
+
+function missaoFail(){
+    console.log(`Você consegue, porém a fuga seu adversário foge.`);
+    console.log(`Frustrado você no maldito hovercar para casa`);
+    console.log(`Chegando em casa ${nomesPersonagens[2]} liga no seu Vidfone e pergunta status da missão.`);
+    console.log(`Você sem frustração alguma em função da anestesia de sentintos que é diz que não teve sucesso `);
+    console.log(`${nomesPersonagens[2]} diz que tudo bem, que a horas desses androides chegaria...`);
+    let escEmpatiaBox = +prompt(`${nomesPersonagens[1]} te convida para usar a caixa de empatia para que se sinta melhor, você aceita? 1 para sim e 2 para não: `);
+    if(escEmpatiaBox==1){//sua esposa o convida para usar a caixa de empatia
+        //aceita
+        console.log(`Você aceita... por que você aceita? Desejo de ver o acontece se ferir? Esperança de que seja melhor agora? Pois é...`);
+        console.log(`Vocês conectam todo o aparato...`);
+        console.log(`Mas a maldita crosta de poeira que está em sua casa gera mau contato com o aparelho...`);
+        console.log(`O aparelho teve mau contato, mas o contato elétrico que a maquina fez com seu cérebro é digno de elogio para a pessoa que frita batatas em fast-food`);
+        console.log(`O cérebro de vocês começa a fritar com o curto circuito da máquina, mas esse momento é estranhamente belo e melancólico como uma pintura de Picasso ao fim da vida.`);
+        console.log(`Sua esposa sorri pois acredita que irá para um lugar como aquela realidade virtual, olha para você com um sorriso como quem quer dizer "te vejo lá meu amor!"`);
+        console.log(`Já você sente que estava certo em não depositar razão ou esperança no real, ele realmente não valeu a pena... Agora, podendo não ser, não existir ao menos não teria de pensar sobre isso`);
+        fim();
+    }
+    else if(escEmpatiaBox==2){
+        //nao
+        console.log(`Você diz para ela que não, que se ela quer ser tolam que seja solo...`);
+        console.log(`Vai descansar, descansa por algumas horas, porém seu vidfone toca no modo emergência`);
+        console.log(`Você atende e lá o ${nomesPersonagens[2]} diz que ${nomesPersonagens[3]},${nomesPersonagens[4]} e ${nomesPersonagens[5]} estão em um prédio abandonado no centro da cidade.`);
+        console.log(`Você vai imediatamente para lá, porém chegando lá é pego de surpresa pelos 3!`)
+        royBattyVengance();
+        fim();
+    }
+}
+
+function lojaDeAnimais(){
+
+}
+
+function androideFoge(){
+
+}
+
+function royBattyVengance(){
+
+}
+
+
+    console.log('Você acaba de acordar de uma noite mal dormida como de costuma');
+    console.log('Sua esposa, Ada Lovelace está com você na cama, e seu animal elétrico, a ovelha Dolly, está a porta.');
     console.log('Com que você interagir nesse despertar? ');
     let esc1 = +prompt('1 para acordar Lovelace com um beijo mórbido e 2 para ir acareciar seu ovelha: ');
     //testaEscolha();
 
     if (esc1 == 2){//narrativa curta//Acordar acariciar animal eletrico
         console.log('Você vai acariciar Dolly, mas ao levantar da cama Lovelace desperta');
-        console.log('Como alguém que passa por um dia fúria ela pega o lustre que esta do seu lado da cama e em furia joga em sua direção!')
+        console.log('Como alguém que passa por um dia fúria ela pega o lustre que esta do seu lado da cama e em furia joga em sua direção!');
         
         
         if (Fnaleatorio < 5){//
@@ -96,9 +146,9 @@ function Fnaleatorio(){
                     
                 } else {
                     Fnaleatorio();
-                    console.log('Ela erra, e no mesmo instante do mais amargo arrependimento de ter se rendido ao seu impulso, Lovelace grita "DESCULPA!!!", mas agora a fúria como um fogarel numa floresta, está ardendo sobre você.')
+                    console.log('Ela erra, e no mesmo instante do mais amargo arrependimento de ter se rendido ao seu impulso, Lovelace grita "DESCULPA!!!", mas agora a fúria como um fogarel numa floresta, está ardendo sobre você.');
                     console.log('Você violentamente avança para cima de Lovelace.');
-                    console.log('E enquanto se afastava de você ela se aproximava da janela do minusculo quarto.')
+                    console.log('E enquanto se afastava de você ela se aproximava da janela do minusculo quarto.');
                     console.log('Ela está na janela, você em furia a empurra');
                     console.log('E assim como a tempestade que apaga o fogo que queimara uma floresta, você é atacado pela consciencia que acabara de retomar.');
                     console.log('Decide que não poderia ser assim, ser, estar numa vida que implica nesse acontecido como real não poderia o ser');
@@ -109,8 +159,8 @@ function Fnaleatorio(){
     else if (esc1 == 1){//Acordar e dar um beijo na Ada Lovelace
         dataHora();
         console.log('Você beija Ada como quem está batendo ponto em um trabalho, na espectativa de que ao final do mês aquilo te recompense, que o futuro seja melhor que o agora');
-        console.log('Agora já desperto, você vai ao banheiro para jogar água no rosto.')
-        console.log('Lá enquanto vê todas as imperfeições física em seu rosto repara de canto algo na janela, mas ao mesmo tempo Ada babucia algo e diz seu nome.')
+        console.log('Agora já desperto, você vai ao banheiro para jogar água no rosto.');
+        console.log('Lá enquanto vê todas as imperfeições física em seu rosto repara de canto algo na janela, mas ao mesmo tempo Ada babucia algo e diz seu nome.');
         let esc2 = +prompt('Você vai falar com Ada (1) ou olha na janela (2) ? ');
 
         if (esc2==2){//acordado/o pombo olha para voce e você nota que é um pombo mecânico
@@ -120,30 +170,30 @@ function Fnaleatorio(){
             console.log('Você vai a janela, e lá vê que tem um pombo batendo asas parado no mesmo lugar...');
             console.log('Parecia até mesmo um glitch de um jogo');
             if(Fnaleatorio <4){
-                Fnaleatorio()
+                Fnaleatorio();
             }
-            console.log('Mas que sentido faz isso... se nem o mais sujo dos animais decreptos não são reais...')
-            console.log('Será que vale mesmo ser Neo... tomar a pílula vermelha?')
-            console.log('A sua conclusão é que sem no o minimo pode ser de verdade, por que então deveria seguir fingindo que vale a pena?')
-            console.log('Ausente uma resposta a si mesmo mesmo você se lança pela janela, sem controle, como que um narrador um programador o fizesse fazer...')
-            console.log('E no contato com o chão, que certamente era real, você deixa de ser, seja o que for que você fosse antes')
+            console.log('Mas que sentido faz isso... se nem o mais sujo dos animais decreptos não são reais...');
+            console.log('Será que vale mesmo ser Neo... tomar a pílula vermelha?');
+            console.log('A sua conclusão é que sem no o minimo pode ser de verdade, por que então deveria seguir fingindo que vale a pena?');
+            console.log('Ausente uma resposta a si mesmo mesmo você se lança pela janela, sem controle, como que um narrador um programador o fizesse fazer...');
+            console.log('E no contato com o chão, que certamente era real, você deixa de ser, seja o que for que você fosse antes');
             fim();
 
 
             }else if (Fnaleatorio>=5){//dormindo/ tenta acordar mas percebe que esta 
-                console.log('O pombo o encara de volta... mas o olha não como um animal preocupado em observar perigos, mas sim em como uma máquina que o sensor sente algo.')
-                console.log('Você então se lembra que nem mesmo pombos, o mais sujo dos animais decreptos não são reais...')
-                console.log('Pse... ser o principal da sua narratiava e sentir o mundo como NPC é o mais puro egocentrismo, mas é a única realidade possível a si.')
-                console.log('E ver esse pombo, esse maldito pombo, reforça que a vida já não vale como construção do real, pois o real se desfez em kg de ferro e cabos. ')
+                console.log('O pombo o encara de volta... mas o olha não como um animal preocupado em observar perigos, mas sim em como uma máquina que o sensor sente algo.');
+                console.log('Você então se lembra que nem mesmo pombos, o mais sujo dos animais decreptos não são reais...');
+                console.log('Pse... ser o principal da sua narratiava e sentir o mundo como NPC é o mais puro egocentrismo, mas é a única realidade possível a si.');
+                console.log('E ver esse pombo, esse maldito pombo, reforça que a vida já não vale como construção do real, pois o real se desfez em kg de ferro e cabos. ');
                 console.log(`Até mais disse ${nomesPersonagens[0]}`)
                 fim();
             }
         else if (esc2==1){//Ada cria diálogo sobre estar triste e se mostra dependente da caixa de empatia
             console.log(`${nomesPersonagens[1]} se diz não triste, más melancólica, não sair do quarto é seu inferno, mas ela mesmo acha que não estar nele é o inferno pior que o de Dante`);
             console.log(`E como um drogado fugindo do real, ${nomesPersonagens[1]} vai atrás de sua Caixa de Empatia`);
-            console.log(`Você odeia a maldita Caixa, já não basta o real ser artifical, quem dira um real propositalmente artificial...`)
+            console.log(`Você odeia a maldita Caixa, já não basta o real ser artifical, quem dira um real propositalmente artificial...`);
             console.log(`Ela te convida a usar dizendo que vai ser bom pra você, que é coisa de sua cabeça...`);
-            let esc3 = +prompt('Você aceita? (digite 1 para sim e 2 para não) ')
+            let esc3 = +prompt('Você aceita? (digite 1 para sim e 2 para não) ');
             if(esc3== 1){//Aceita/Entram juntos, é descrito com funciona (descrição onirica e 
                 console.log(`Com a Caixa em mãos, ela pega os conectores e os coloca no corpo de ${nomesPersonagens[0]}. `);
                 console.log(`Ela, que tão raro quanto a ausencia de fumaça ficava feliz e empolgada, o ficou pois iria te apresentar o Oasis`);
@@ -159,8 +209,21 @@ function Fnaleatorio(){
                 
             }if(esc3==2){//Não aceitam/Tem uma briga/
                 console.log(`Diz que não é um viciado, que somente pessoas baratas se poem a utilizar esta maldita Caixa de Empatia`);
-                console.log('Você para o trabalho')
+                console.log('Você para o trabalho');
             }
+
+            console.log(`Chegando no trabalho no trabalho seu chefe,${nomesPersonagens[2]} te passa uma missão para ir atrás de dois androides.`);
+            console.log(`A primeira é ${nomesPersonagens[4]}, uma adroide com "espirito" selvagem, cintilante como Cyndi Lauper nos anos 80 `);
+            console.log(`O outro é ${nomesPersonagens[5]} um androide com aspirações a ser um Terminator no primeiro filme, metódico e de poucas palavras.`);
+            let esc4= +prompt(`Quem você vai atrás? Digite 1 para ${nomesPersonagens[4]} e 2 para ${nomesPersonagens[5]}: `);
+            if (esc4==1){
+                console.log(`${nomesPersonagens[2]} te diz que documentos indicam o paradeiro dela na Rua Donald Knuth ou Rua Grace Murray Hopper. Porém você também queria ir na loja de animais elétricos ver as novidades `);
+
+            }
+            else if(esc4==2){
+                console.log(`${nomesPersonagens[2]} te diz que documentos indicam o paradeiro dele na Rua Vint Cerf ou Rua Tim Berners-Lee. Porém você também queria ir na loja de animais elétricos ver as novidades`);
+            }
+
             //*******como vou fazer para 2 caminhos disitintos se conectarem????*******************/ 
             //chamar um function e continuar a partir dela??
             //function "continua_historia() ?""   
