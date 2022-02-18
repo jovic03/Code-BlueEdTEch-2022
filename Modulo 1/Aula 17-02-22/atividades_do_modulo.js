f2994846ba6a89b5381f5c8b464557a1ec56c0d8
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-02-17 22:22:15 -0300

    main

--- Modulo 1/Aula 17-02-22/atividades_do_modulo.js
+++ Modulo 1/Aula 17-02-22/atividades_do_modulo.js
@@ -42,7 +42,7 @@ console.log(`Voce viveu dias ${calDias} de vida`);
 */
 
 /* 4 -Faça o cálculo contrário: Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias. Também considere todo ano com 365 dias e todo mês com 30 dias.*/
-
+/*
 let dias = +prompt('Quantos dias vc tem? ');
 
 let calcyy = Math.floor(dias/365);
@@ -52,3 +52,34 @@ let calcmm = Math.floor((dias%365)/30);
 let caldd = ((dias%365)%30)
 
 console.log(dias, calcyy, calcmm, caldd)
+*/
+
+/*
+
+1) Frase na tela - Implemente um programa que escreve na tela a frase "O primeiro programa a gente nunca esquece!".
+*/
+/*
+console.log('O primeiro programa a gente nunca esquece!".')
+*/
+
+/*2) Etiqueta - Elabore um programa que escreve seu nome completo na primeira linha, seu endereço na segunda, o CEP na terceira e o telefone na quarta.*/
+
+/*3) Letra de Música - Faça um programa que mostre na tela uma letra de música que você gosta.*/
+
+/*4) Tabela de notas - Você foi contratado ou contratada por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+ALUNO (A) || NOTA
+ALINE || 9.0
+MÁRI0 || 10
+SÉRGIO || 4.5
+SHIRLEY || 7.0
+*/
+
+/*5) Faça um programa de cadastro de clientes que mostre um menu de opções e permita com que a pessoa escolha umas das opções, exibindo qual foi a opção escolhida.*/
+
+/*6) Calculadora de Dano - Escreva um programa que receba dois valores digitados pelo usuário: quantidade de vida de um monstro (entre 10 e 50) e valor do ataque do jogador por turno (entre 5 e 10). Baseado nos valores digitados, exiba a quantidade de turnos que o jogador irá demorar para conseguir derrotar o monstro (o jogador irá derrotar o monstro em 8 turnos).*/
+
+/*7) E os 10% do garçom? - Defina uma variável para o valor de uma refeição que custou R$ 42,54. Na sequência, defina uma variável para o valor da taxa de serviço que é de 10%. Por fim defina uma variável que será responsável pelo cálculo do valor total da conta e exiba-o no console com a seguinte formatação: R$99.99 (valor com duas casas decimais).*/
+
+/*8) Qual o valor do troco? - Crie um programa que calcule o troco de uma compra no valor de R$100,98 e que foi paga com R$150,00. O valor do troco deve ser exibido no console.*/
+
+/*9) Conversor de moedas - Crie um programa que solicite um um valor em real ao usuário e converta esse valor, para dólar.*/
