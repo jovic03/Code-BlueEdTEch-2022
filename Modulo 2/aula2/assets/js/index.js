1acafb742b76d34f58e724332c35cabf8939348c
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-03-10 21:13:10 -0300

    main

--- Modulo 2/aula2/assets/js/index.js
+++ Modulo 2/aula2/assets/js/index.js
@@ -1,25 +1,26 @@
+const btnAltHumor = document.querySelector('#botao-alteraHumor')// Captura o botão no DOM pelo id
 const elementoNome = document.getElementById('nome'); /* Cria a variável elementoNome e coloca o elemento com o id nome nela */
-const elementoSituacao = document.querySelector('#situacao'); /* Cria a variável elementoSituacao e coloca o elemento com o id situacao nela */
-const elementoImg = document.querySelector('#imagem') /* Cria a variável elementoImg e coloca o elemento com o id imagem nela */
-let elementoBtn = document.querySelector('#alterar') /* Cria a variável elementoBtn e coloca o elemento com o id alterar nela */
+const alteraSituacao = document.querySelector('#situacao'); /* Cria a variável alteraSituacao e coloca o elemento com o id situacao nela */
+const alteraImagem = document.querySelector('#imagem') /* Cria a variável alteraImagem e coloca o elemento com o id imagem nela */
+let elementoBtn = document.querySelector('#botao-alteraHumor') /* Cria a variável elementoBtn e coloca o elemento com o id alterar nela */
 
 /* Adiciona o evento de click no elementoBtn e executa a função anonima (arrow function) */
 elementoBtn.addEventListener('click', () =>{
     /* Verifica se o valour do botão é 'primeiro', se for troque de humor para o Hulk */
     if(elementoBtn.value == 'primeiro'){
-        elementoImg.src = './assets/img/smile2.gif' /* Troca a imagem do personagem */
-        elementoNome.innerText = 'Sorriso' /* Altera o texto do elemento nome */
-        elementoSituacao.innerText = 'Sorrindo :)' /* Altera o texto do elemento situação */
+        alteraImagem.src = './assets/img/smile2.gif' /* Troca a imagem do personagem */
+        elementoNome.innerText = 'Emoji Feliz' /* Altera o texto do elemento nome */
+        alteraSituacao.innerText = 'Feliz' /* Altera o texto do elemento situação */
         elementoBtn.value = 'segundo' /* Altera valor do botão */
     } else if(elementoBtn.value == 'segundo') { /* Verifica se o valour do botão é 'segundo', se for troque de humor para o Dr. Hulk */
-        elementoImg.src = './assets/img/angry2.gif'  /* Troca a imagem do personagem */
-        elementoNome.innerText = 'Bravo' /* Altera o texto do elemento nome */
-        elementoSituacao.innerText = 'Pistola' /* Altera o texto do elemento situação */
+        alteraImagem.src = './assets/img/angry2.gif'  /* Troca a imagem do personagem */
+        elementoNome.innerText = 'Emoji Pistola' /* Altera o texto do elemento nome */
+        alteraSituacao.innerText = 'Bravo' /* Altera o texto do elemento situação */
         elementoBtn.value = 'terceiro'  /* Altera valor do botão */
     } else { /* Se os valores do botão não forem, 'primeiro' nem 'segundo' mude para o Dr. Banner */
-        elementoImg.src = './assets/img/nerd.gif' /* Troca a imagem do personagem */
-        elementoNome.innerText = 'nerd' /* Altera o texto do elemento nome */
-        elementoSituacao.innerText = 'Nerd' /* Altera o texto do elemento situação */
+        alteraImagem.src = './assets/img/nerd.gif' /* Troca a imagem do personagem */
+        elementoNome.innerText = 'Nerdola' /* Altera o texto do elemento nome */
+        alteraSituacao.innerText = 'A estudar' /* Altera o texto do elemento situação */
         elementoBtn.value = 'primeiro' /* Altera valor do botão */
     }
 })
