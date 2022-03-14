1d800b2156672596b73735c2f220107b23ec484c
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-03-14 20:28:44 -0300

    main

---
+++ Modulo 2/aula6/aula6pt1/js/index.js
@@ -0,0 +1,19 @@
+let h1 = document.querySelector('h1')//o nome da variavel poderia ser qualquer um, mas boa praticas é utilizado o mesmo nome da tag que vai ser modificado
+// h1.innerHTML = 'mudei o nome'
+
+let img = document.querySelector('img')
+
+// img import '../assets/img/' -- dica de achar a pasta
+
+let a = document.querySelector('a')
+
+console.log(h1)
+
+function alterarTexto(){
+    // h1.id = 'meu-novo-id'
+    // h1.innerHTML = 'mudei o nome'
+    //h1.style.color='red'
+
+    // img.src = './assets/img/img2.jpg'
+
+}
