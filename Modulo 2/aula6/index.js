8154e7483ad2c2ae33de007d133a5e6b107fad6b
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-03-15 21:44:22 -0300

    main

--- Modulo 2/aula6/index.js
+++ Modulo 2/aula6/index.js
@@ -1,1 +1,20 @@
-console.log('testando')
+//imprtando o modulo express
+import express from 'express'
+
+//criar uma variavek e instanciar (ou seja: passar todas as funcoes da funcao expresse para a variavel app)
+const app = express()
+//informa para o meu express que o motor de visualizacao (ou seja, o modo que vai exibir as minhas paginas ) vai ser EJS
+app.set('view engine','ejs')
+
+//variavel porta = indica qual porta o nosso setvifor vai rodar
+const port = 3001
+
+/*
+
+*/
+
+app.listen(port, () =>{
+    console.log(`Meu servidor esta rodando na porta ${port}`)
+})
+
+console.log('testandinho')
