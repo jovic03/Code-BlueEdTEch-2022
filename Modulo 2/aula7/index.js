1ed33a152258c3b8f84477bdd40e2b3032c7a06c
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-03-16 19:11:31 -0300

    main

--- Modulo 2/aula7/index.js
+++ Modulo 2/aula7/index.js
@@ -1,9 +1,12 @@
+// Importando o moódulo o express
 import express from 'express'
 
+// Criar uma variável e instanciar (ou seja: passar todos as funções da função express para o variável app)
 const app = express()
-
+// Informa para o meu express que o motor de visualização (ou seja, o modo que vai exibir as minhas páginas) vai ser o EJS
 app.set('view engine', 'ejs')
 
+// Variável porta = indica qual porta o nosso servidor vai rodar
 const port = 3000
 
 app.listen(port, () => {
