c352481c75297353cf0e1eaf5ae29a17d0521951
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-03-18 20:24:36 -0300

    main

--- Modulo 2/Pokedex3/index.js
+++ Modulo 2/Pokedex3/index.js
@@ -1,1 +1,22 @@
 import express from 'express'
+
+const app =express()
+app.set('view engine', 'ejs')//tudo que for ejs dentro da pasta views seja renderizado
+
+const port = 3000
+
+app.listen(port, (req,res)=>{ //configurando servidor
+
+    console.log(`Estou rodando na porta ${port}`)
+})
+
+//criando rotas
+
+app.get('/',(req,res)=>{//nome da rota e depois func anonima (callback)
+    //res.send('Ola')
+    res.render('index.ejs')//renderiza o index.ejs pro usuario
+})
+
+app.get('/detalhes',(req,res)=>{//nome da rota e depois func anonima (callback)
+    res.render('detalhes.ejs')//renderiza o index.ejs pro usuario
+})
