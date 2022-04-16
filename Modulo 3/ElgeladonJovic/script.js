9ffb34552814e0057cca8dbb2f11dc92d957b886
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-04-16 18:33:05 -0300

    configurando o cadstro de paletas (html, script e css)

--- Modulo 3/ElgeladonJovic/script.js
+++ Modulo 3/ElgeladonJovic/script.js
@@ -16,7 +16,7 @@ async function findAllPaletas() {/*como é fora do front e pra nao ter problema 
         <div class="PaletaListaItem">
             <div>
               <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
-              <div class="PaletaListaItem__preco">R$${paleta.preco.toFixed(2)}</div>
+              <div class="PaletaListaItem__preco">R$${paleta.preco}</div>
               <div class="PaletaListaItem__descricao">${paleta.descricao}</div>
             </div>
             <img class="PaletaListaItem__foto" src=${paleta.foto} alt=${`Paleta de ${paleta.sabor}`} />
@@ -29,6 +29,29 @@ async function findAllPaletas() {/*como é fora do front e pra nao ter problema 
   
 }
 
+//Pegar uma paleta pelo ID
+const findByIdPaletas = async () => {
+  const id = document.getElementById("idPaleta").value;
+
+  const response = await fetch(`${baseUrl}/find-paleta/${id}`);
+
+  const paleta = await response.json();
+
+  const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
+
+  paletaEscolhidaDiv.innerHTML = 
+  `
+  <div class="PaletaCardItem">
+    <div>
+      <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
+      <div class="PaletaCardItem__preco">R$${paleta.preco}</div>
+      <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
+    </div>
+      <img class="PaletaCardItem__foto" src=${paleta.foto} alt=${`Paleta de ${paleta.sabor}`} />
+  </div>
+  `;
+};
+
 findAllPaletas();
 
 function abrirModalCadastro(){
@@ -37,29 +60,61 @@ function abrirModalCadastro(){
 
 function fecharModalCadastro(){
   document.querySelector('.modal-overlay').style.display = 'none';/*procure ".modal-overlay" em .style e procure a propriedade display e altere pra o seu valor paro o texto : "none"*/
+  
+  document.querySelector('#sabor').value = "";
+  document.querySelector('#preco').value = 0;
+  document.querySelector('#descricao').value = "";
+  document.querySelector('#foto').value = "";
+}
+
+async function createPaleta(){
+  //event.preventDefault();/* tira o comportanto padrao do botao de tipo submit */
+
+  const sabor = document.querySelector('#sabor').value;
+  const preco = document.querySelector('#preco').value;
+  const descricao = document.querySelector('#descricao').value;
+  const foto = document.querySelector('#foto').value;
+
+  const paleta = {
+    sabor,
+    preco,
+    descricao,
+    foto
+  };
+  const response = await fetch(baseUrl + "/create",{/*essa é a resposta*/
+    method:"post",
+    headers:{
+      "Content-Type":"application/json",/*qual a forma de envio do conteudo (no caso via json)*/
+    },
+    mode:"cors",/*cors permite acessar o conteudo do back*/
+    body: JSON.stringify(paleta),/*estou enviado atraves do body o conteudo do json do const paleta da linha 48 */
+  });
+
+  const novaPeleta = await response.json();
+
+  const html =
+    `
+    <div class="PaletaListaItem">
+        <div>
+          <div class="PaletaListaItem__sabor">${novaPeleta.sabor}</div>
+          <div class="PaletaListaItem__preco">R$${novaPeleta.preco}</div>
+          <div class="PaletaListaItem__descricao">${novaPeleta.descricao}</div>
+        </div>
+        <img class="PaletaListaItem__foto" src=${novaPeleta.foto} alt=${`Paleta de ${novaPeleta.sabor}`} />
+
+    </div>
+    `;
+
+    document.querySelector('#paletaList').insertAdjacentHTML('beforeend',html);
+
+    //zerando todos os valores do form depois de ter dado "submit"--update: agora que esta sendo feito no fecharModalcadastro, e toda vez que cadastramos algo ele ja fecha nao é necessário estar aqui
+    /*document.querySelector('#sabor').value = "";
+    document.querySelector('#preco').value = 0;
+    document.querySelector('#descricao').value = "";
+    document.querySelector('#foto').value = "";*/
+
+    fecharModalCadastro();
 }
 
 
-//Pegar uma paleta pelo ID
 
-const findByIdPaletas = async () => {
-    const id = document.getElementById("idPaleta").value;
-  
-    const response = await fetch(`${baseUrl}/find-paleta/${id}`);
-  
-    const paleta = await response.json();
-  
-    const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
-  
-    paletaEscolhidaDiv.innerHTML = 
-    `
-    <div class="PaletaCardItem">
-      <div>
-        <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
-        <div class="PaletaCardItem__preco">R$${paleta.preco.toFixed(2)}</div>
-        <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
-      </div>
-        <img class="PaletaCardItem__foto" src=${paleta.foto} alt=${`Paleta de ${paleta.sabor}`} />
-    </div>
-    `;
-};
