23b26b39fef2481245aebf186d3a0efe8e6c218c
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-04-22 21:26:51 -0300

    Validando agora pelo Middleware

--- Modulo 3/ElGeladonVersao2/src/controllers/paletas.controller.js
+++ Modulo 3/ElGeladonVersao2/src/controllers/paletas.controller.js
@@ -3,15 +3,20 @@ const paletasService = require('../services/paletas.service');
 
 const findPaletasController = async (req, res)=>{
     const allPaletas = await paletasService.findPaletaService();//requisicao de todas as peletas
+
+    if(allPaletas.length == 0){
+        return res.status(400).send({message:'Nao há paletas cadastradas'})
+    }
+
     res.send(allPaletas);//envia a req
 }
 
 const findPaletaByIdController = async (req,res)=>{
     const idParam = req.params.id;//params é funcao do req
 
-    if(!mongoose.Types.ObjectId.isValid(idParam)){//types vem do mongo
+    /*if(!mongoose.Types.ObjectId.isValid(idParam)){//types vem do mongo
         res.status(400).send({message:'ID Invalido'});//se nao for valido senda essa mensagem
-    }
+    }*/
 
     const chosenPaleta = await paletasService.findPaletaByIdService(idParam);//aqui retorna a paletas escolhida
 
@@ -30,29 +35,29 @@ const findPaletaByIdController = async (req,res)=>{
 const createPaletaController = async  (req,res)=>{
     const paleta = req.body;//sera recebido pelo json abaixo
     
-    if(
+    /*if(
         !paleta ||
         !paleta.sabor ||
         !paleta.descricao ||
         !paleta.foto ||
         !paleta.preco){
             return res.status(400).send({ message: "Você não preencheu todos os dados para adicionar a paleta!" })
-    }
+    }*/
     
     const newPaleta = await paletasService.createPaletaService(paleta);
-    res.send(newPaleta);
+    res.status(201).send(newPaleta);
 }
 
 const updatePaletaController = async (req,res)=>{
     const idParam = req.params.id;//recebe nas duas linhas
     const paletaEdit = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(idParam)){
+    /*if(!mongoose.Types.ObjectId.isValid(idParam)){
         res.status(400).send({message:'Id invalido'});
-    }
+    }*/
 
     //validando se exite uma paleta com o ID passado
-    const chosenPaleta = await paletasService.findPaletaByIdService(idParam);
+    /*const chosenPaleta = await paletasService.findPaletaByIdService(idParam);
 
     if(!chosenPaleta){
         return res.status(400).send({message:'Paleta não cadastrada'});
@@ -66,7 +71,7 @@ const updatePaletaController = async (req,res)=>{
         !paletaEdit.foto ||
         !paletaEdit.preco){
             return res.status(400).send({ message: "Você não preencheu todos os dados para editar a paleta!" })
-    }
+    }*/
 
     /*if(!idParam){
         return res.status(400).send({message:'Paleta nao encontrada'});
@@ -82,15 +87,15 @@ const deletePaletaController= async (req,res)=>{
 
     const idParam = req.params.id;
 
-    if (!mongoose.Types.ObjectId.isValid){
+    /*if (!mongoose.Types.ObjectId.isValid){
         return res.status(400).send({message:'Id invalido'});
-    }
+    }*/
 
     const chosenPaleta =  paletasService.findPaletaByIdService(idParam);
 
-    if(!chosenPaleta){
+    /*if(!chosenPaleta){
         return res.status(400).send({message:'Paleta não encontrada'})
-    }
+    }*/
 
     await paletasService.deletePaletaService(idParam);
 
--- Modulo 3/ElGeladonVersao2/src/routes/paletas.routes.js
+++ Modulo 3/ElGeladonVersao2/src/routes/paletas.routes.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const router = express.Router();//ajuda criar rotas dentro do arquivo
 
+const {validId,
+    validObjectBody} = require('../middlewares/paleta.middleware');
+
 const paletasController = require('../controllers/paletas.controller');
 
 //rota para listar paletas (no primeiro todos e no segundo pelo id especifico)
-router.get('/find-paletas',paletasController.findPaletasController);
-router.get('/find-paletas/:id',paletasController.findPaletaByIdController);
+router.get('/all-paletas',paletasController.findPaletasController);
 
-//rota para criar paleta
-router.post('/create',paletasController.createPaletaController);
+//rota para listar uma paleta por ID
+router.get('/one-paletas/:id',validId, paletasController.findPaletaByIdController);
 
-//rota para deletar paleta pelo id
-router.delete('/delete/:id',paletasController.deletePaletaController);
+//rota para criar paleta--por ele receber json ele precisa do middleware
+router.post('/create-paleta',validObjectBody,paletasController.createPaletaController);
+
+// Usaremos o PUT para atualizar todo o recurso, o PATCH pode ser usado para atualizar partes de um registro
+router.put('/update-paleta/:id',validId,validObjectBody, paletasController.updatePaletaController);
+
+//rota para deletar paleta pelo id--por ele receber json ele precisa do middleware
+router.delete('/delete-paleta/:id',validId,paletasController.deletePaletaController);
 
 //cria um dado e substitui por um novo-- usaremos o put para atualizar todo o recurso, p tach pode ser usado para atualizar parte de um registro
 //router.put('/update/:id',paletasController.updatePaletaController);
