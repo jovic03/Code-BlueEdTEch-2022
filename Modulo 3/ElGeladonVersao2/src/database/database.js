b526ef09bfebebb7ce3d96fe5bd58d10aec210e6
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-04-26 19:38:19 -0300

    Update database.js

--- Modulo 3/ElGeladonVersao2/src/database/database.js
+++ Modulo 3/ElGeladonVersao2/src/database/database.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 //mongodb://localhost:27017/paletas-db
 
 const connectToDatabase = () => {// mongodb://localhost:27017/paletas-db  ---- mongodb+srv://localhost:27017/paletas-db
-    mongoose.connect('mongodb://localhost:27017/paletas-db',{//os campos abaixo são segurando a estabilidade de conexao de rede
+    mongoose.connect('mongodb://localhost:28017/paletas-db',{//os campos abaixo são segurando a estabilidade de conexao de rede
         useNewUrlParser: true,//use esse sistema de url
         useUnifiedTopology: true,//ferramente de monitoramento de dados
       })
