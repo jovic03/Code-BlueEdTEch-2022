7c5ca8cfdd668bb5e484989924705f74596d1ce9
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-04-27 21:51:23 -0300

    indicador de adicao de produto

--- Modulo 3/ElGeladonVersao2/cardapio-el-geladon/src/PaletaLista.jsx
+++ Modulo 3/ElGeladonVersao2/cardapio-el-geladon/src/PaletaLista.jsx
@@ -6,23 +6,24 @@ function PaletaLista() {
 
   const [paletaSelecionada, setPaletaSelecionada] = useState({});
 
-  /*
+  
   const adicionarItem = (paletaIndex) => {
     const paleta = { [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) +1 }
     setPaletaSelecionada({ ...paletaSelecionada, ...paleta});
-  }*/
+  }
 
     return (
       <div className="PaletaLista">{/*tem que ser chave pois aqui é js---paleta pega uma paleta no paletas.js e o index serve pra mapear o indice e serve pro js */}
       {/* for (int i = 0;i<20;i++): */}
         {paletas.map((paleta,index) =>
             <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>{/* key serve para atribuir a cada elemnto um valor unico */}
+              <span className='PaletaListaItem__badge'>{paletaSelecionada[index] || 0}</span>
                     <div>
                       <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
                       <div className="PaletaListaItem__preco">R$ {paleta.preco.toFixed(2)}</div>
                       <div className="PaletaListaItem__descricao"> {paleta.descricao}  </div>
                       <div className="PaletaListaItem__acoes Acoes">
-                        <button className="Acoes__adicionar Acoes__adicionar--preencher">adicionar</button>
+                        <button className="Acoes__adicionar Acoes__adicionar--preencher" onClick={() => adicionarItem(index)}>adicionar</button>
                       </div>
                     </div>
                     <img className="PaletaListaItem__foto" 
