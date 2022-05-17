1992d9e8de96000fc19577faa34998938c38cee5
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-05-17 18:41:27 -0300

    Update index.ts

--- Modulo 4/ElGeladonVers2/index.ts
+++ Modulo 4/ElGeladonVers2/index.ts
@@ -1,3 +1,12 @@
-function log(constructor:any){
+function log(msgem:string){
+  return function (constructor:any){
+    console.log(msgem);
+  }  
+}
+
+@log("Fui chamado")
+class Blue{
 
 }
+
+const objetoDaClasseBlue = new Blue(;)
