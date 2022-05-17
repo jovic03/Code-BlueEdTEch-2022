3ba4fb5363b8742db2881d7e612c4afb143a6555
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-05-17 18:35:13 -0300

    update

--- Modulo 4/ElGeladonVers2/index.ts
+++ Modulo 4/ElGeladonVers2/index.ts
@@ -1,32 +1,3 @@
-/*
-npm init (inica ambiente de codar)
-npm install -D typescript (inica ambiente de codar)
-npx tsc --init (inica ambiente de codar)
-npx tsc (traduz pra js o codigo, toda vez rodar ele antes)
-node index.js (quem de fato vai rodar)
+function log(constructor:any){
 
-package.json deve ficar:
- "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "start":"node index.js",*****
-    "build":"npx tsc",*****
-    "build-and-run":"npm run build && npm start"*****
-  },
-
-
-npm *run* build pra rodar o build (nao somente npm build como é com npm star);
-
-npm *run* build-and-run 
-*/
-
-type Filme = {
-    titulo: string;
-    lancamento: number;
 }
-
-const HarryPotter: Filme={
-    titulo:"Harry Potter 8",
-    lancamento:2013
-}
-
-console.log(HarryPotter)
